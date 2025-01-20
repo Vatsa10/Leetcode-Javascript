01c3e682a2b2a5eccda4567fce5ddc7bc43f8ae8
Vatsa Joshi <123623531+Vatsa10@users.noreply.github.com>
2025-01-20 16:00:37 +0530

    Create 2665.js
    Question 4

---
+++ 2665.js
@@ -0,0 +1,16 @@
+//Question 4
+var createCounter = function(init) {
+    const n = init
+       function increment(){
+           return init += 1
+        }
+       function decrement(){
+            return init -= 1
+        }
+       function reset(){
+            return init = n
+        }
+    return {
+        increment , decrement , reset
+    }
+};

7114ce91b9c7a17f0c52b6941685c2975cf37f15
Vatsa Joshi <123623531+Vatsa10@users.noreply.github.com>
2025-01-21 21:22:50 +0530

    Create 2634.js

---
+++ 2634.js
@@ -0,0 +1,11 @@
+//Question 6
+
+var filter = function(arr, fn) {
+    let newarr = []
+    for(let i=0; i < arr.length; i++){
+        if(fn(arr[i],i)){
+            newarr.push(arr[i])
+        }
+    }
+    return newarr
+};
